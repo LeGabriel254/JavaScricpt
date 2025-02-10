52071674440e5d04edc6e36ddaa8cf80ac65729d
Leon Gabriel <g73346920@gmail.com>
2025-02-10 22:10:11 +0300

    Initial commits

--- Day 1/variables/variables.js
+++ Day 1/variables/variables.js
@@ -3,3 +3,14 @@
 
 // var is function-scoped that was traditionally to declare variables used while let and const
 // are both block-scoped whereby let allows reasignment and const allows only unchanged variable 
+
+
+const age = 25;
+let name = "Leon";
+
+let John = 4;
+let sam = 5;
+
+alert(John + sam)
+
+console.log(name)
