c422c0a130ddb787a48ce31037f6dc41bef3c70b
Leon Gabriel <g73346920@gmail.com>
2025-02-11 12:29:27 +0300

    Modified

--- Day 1/variables/variables.js
+++ Day 1/variables/variables.js
@@ -4,7 +4,7 @@
 // var is function-scoped that was traditionally to declare variables used while let and const
 // are both block-scoped whereby let allows reasignment and const allows only unchanged variable 
 
-const age = 25; // Declares a constant variable
+const ages = 25; // Declares a constant variable
 let name = "Leon"; // Declares a mutable variable
 
 let John = 4; // Declares a variable with value 4
@@ -13,3 +13,17 @@ let sam = 5;  // Declares a variable with value 5
 alert(John + sam); // Shows an alert with the sum (4 + 5 = 9)
 
 console.log(name); // Logs "Leon" to the console
+
+
+let message;
+
+message = "I love Lyon"
+console.log(message)
+
+
+//multiple variables
+const user = "Leon"
+const  age = 22;
+const school = "Alx africa";
+
+console.log(`My data are: ${user} from ${school} ${age}`)
