efd4415b8ec0e5188abd0ba3a6d9178f38c026f7
Leon Gabriel <g73346920@gmail.com>
2025-02-11 13:52:36 +0300

    worked on strings

---
+++ Day 1/dataTypes/dataTypes.js
@@ -0,0 +1,40 @@
+// Data Types :this is the latest ECMAScript defines 8 data types.
+
+// 7 of them are Primitive data types(values that ais not an object and has no method or properties)
+//Number
+// String
+//Boolean
+//null
+//bigint
+//symbols
+//undefined
+//object
+
+
+//Strings = are text that should be inside a single or double quotes.
+
+let name = "Leon"
+//  Embed a variable
+alert(`Hello ${name}!`);
+
+
+//embed an expression
+alert(`the result is ${4 + 6}`);
+
+//String concatenation
+let fisrtName = "Leon"
+let lastName = "Gabriel"
+
+let fullName = fisrtName + " " + lastName
+console.log(fullName);
+
+// how to get a character in string
+console.log(fisrtName[0]);
+
+//how to know the length
+console.log(fullName.length)
+
+
+
+//The Number this include both numbers and integers
+
