ec6fd480b4f7b9658c19bf3386c1dca6db6ec24a
Leon Gabriel <g73346920@gmail.com>
2025-02-13 13:46:54 +0300

    Chored:conditional statement used

---
+++ Day 2/index.js
@@ -0,0 +1,69 @@
+// Control Structures and Basic Functions
+
+// Conditional statement - they are used switch statement to execute code based on different condition
+
+// if = used to execute block of code if the condition is true
+// else = used with if to execute code if the condition is false 
+// else if = specify new condition if the first condition is false.
+
+
+// Loops - used to execute code repeatedly ased on condition
+
+// for  
+// while 
+// do while
+
+
+// if 
+
+// let age = Number(prompt("How old are you"))
+ 
+// if age is bigger than 20 user is allowed
+// if(age > 20){
+//   alert("You are allowed")
+// }
+
+// let year =Number(prompt("Which year is this"))
+//  // if year is 2015 return you are good
+// if(year == 2015){
+//   alert("You are good")
+// }
+
+// else .. if
+// let age = Number(prompt("How old are you"))
+ 
+// // if age is bigger than 20 user is allowed and if is less than 20 user is not allowed 
+// if(age > 20){
+//   alert("You are allowed")
+// } else if(age < 20) {
+//  alert("You are not allowed")
+// }
+
+
+// let year =Number(prompt("Which year is this"))
+//  // if year is 2015 return you are good if not you are wrong
+// if(year == 2015){
+//   alert("You are good")
+// } else if (year !== 2015){
+//   alert("You are wrong ")
+// }
+
+// else 
+let age = Number(prompt("How old are you"))
+ 
+// if age is bigger than 20 user is allowed and if is less than 20 user is not allowed 
+if(age > 20){
+  alert("You are allowed")
+} else if(age < 20) {
+ alert("You are not allowed")
+} else {
+  alert("we will considor you")
+}
+
+let year =Number(prompt("Which year is this"))
+// if year is 2015 return you are good if not you are wrong
+if(year == 2015){
+ alert("You are good")
+} else {
+  alert("You are wrong")
+}
