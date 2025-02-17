d5345e8a2784327400eb6d1ff2afd76b8be52089
Leon Gabriel <g73346920@gmail.com>
2025-02-17 23:27:26 +0300

    Chored :worked with the for loop and understood that  is used when one knows the times of loops

---
+++ Day 3/loops.js
@@ -0,0 +1,14 @@
+// Loops - loops are used in programming to repeat a block of code.
+
+// For - is used when one knows the times of loop
+
+// for (let i = 1; i <= 6; i++) {
+//   console.log(`Leon is an enginer ${i}`)
+// }
+
+/*looping through an array*/
+let coding = ["JavaScript", "TypeScript", "Python", "Java"]
+
+for (let i = 0; 1 < coding.length; i++) {
+  console.log(`${coding[1]} is a programming language`)
+}
