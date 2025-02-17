d1ffa14529a6436c396c69382bae628b71fb35be
Leon Gabriel <g73346920@gmail.com>
2025-02-17 23:31:33 +0300

    Choored:loops through arrays example above

--- Day 3/loops.js
+++ Day 3/loops.js
@@ -5,10 +5,12 @@
 // for (let i = 1; i <= 6; i++) {
 //   console.log(`Leon is an enginer ${i}`)
 // }
-
+  
 /*looping through an array*/
-let coding = ["JavaScript", "TypeScript", "Python", "Java"]
+let coding = ["JavaScript", "TypeScript", "Python", "Java"];
 
-for (let i = 0; 1 < coding.length; i++) {
-  console.log(`${coding[1]} is a programming language`)
+for (let i = 0; i < coding.length; i++) {
+  console.log(`${coding[i]} is a programming language`);
 }
+
+
